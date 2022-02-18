454e2977f802b19d067f1bfb4b61811e1fcf0289
Daniel Mao <daniel.mao@gusto.com>
2022-02-17 18:24:26 -0800

    working version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.css';
 
-const app = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+console.log('hi')
+const App = () => {
+    console.log('hi')
+    return <div>hello world</div>
 }
 
-export default app;
+export default App;
